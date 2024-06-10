c70cd012082b9a22aeb8e931e059aa5f070538e8
Azat222 <azat222@gmail.com>
2024-06-10 22:26:43 +0300

    feat: add isa

---
+++ ak_lab3/isa.py
@@ -0,0 +1,37 @@
+import json
+from enum import Enum
+
+
+class Opcode(str, Enum):
+    DUP = "dup"
+    OVER = "over"
+    POP = "pop"
+    SWAP = "swap"
+    LOAD = "load"
+    SAVE = "save"
+    INC = "inc"
+    DEC = "dec"
+    ADD = "add"
+    SUB = "sub"
+    DIV = "div"
+    XOR = "xor"
+    MUL = "mul"
+    JUMP = "jump"
+    JZ = "jz"
+    PUSH = "push"
+    HALT = "halt"
+
+    def __str__(self):
+        return self.value
+
+
+def write_code(filename: str, code: list[Opcode]) -> None:
+    with open(filename, "w", encoding="utf-8") as file:
+        buf = []
+        for instr in code:
+            buf.append(json.dumps(instr))
+        file.write("[" + ",\n ".join(buf) + "]")
+
+
+def read_code(filename: str) -> dict[str, str]:
+    raise NotImplementedError()
